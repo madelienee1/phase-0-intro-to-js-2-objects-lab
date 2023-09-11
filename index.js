4aee3ccede3e63660a4e827643178de682a08e55
Madeliene <madelienestiller@gmail.com>
2023-09-11 19:29:52 +1000

    objects lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+        name: "Madeliene",
+        streetAddress: "Maroochydore",
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const nonDestructive = {...employee, [key] :value};
+    
+    return nonDestructive;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const deleteEmployee = { ...employee };
+    deleteEmployee[key] = value;
+    return deleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
