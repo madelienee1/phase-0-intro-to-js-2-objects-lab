718d35e9d449a91d9742d0bd31ec8058d3b34572
Madeliene <madelienestiller@gmail.com>
2023-10-25 09:40:07 +1000

    labfor phase 1 complete

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+
+
 const employee = {
-        name: "Madeliene",
-        streetAddress: "Maroochydore",
-}
+    name: "Maddy",
+    streetAddress: "Maroochydore",
+};
 
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const nonDestructive = {...employee, [key] :value};
-    
+    const nonDestructive = { ...employee, [key]: value };
     return nonDestructive;
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value
+    employee[key] = value;
     return employee;
 }
 
-function deleteFromEmployeeByKey(employee, key, value){
+function deleteFromEmployeeByKey(employee, key, value) {
     const deleteEmployee = { ...employee };
     deleteEmployee[key] = value;
     return deleteEmployee;
@@ -23,5 +24,5 @@ function deleteFromEmployeeByKey(employee, key, value){
 
 function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
     employee[key] = value;
-    return employee
+    return employee;
 }
